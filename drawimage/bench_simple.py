cb18fc0adbd93ecb096f316dfc887a53d7fd4f9b
Eugene Morozov <jmv@emorozov.net>
2022-11-24 16:02:06 +0200

    Adds pyvips to the comparison.

--- drawimage/bench_simple.py
+++ drawimage/bench_simple.py
@@ -1,6 +1,7 @@
 import subprocess
 from io import BytesIO
 
+import pyvips
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -30,8 +31,16 @@ def draw_pillow():
         )
     image.save(open('output.jpg', 'wb'), format='jpeg')
 
+def draw_vips():
+    """Draw text on image using pyvips."""
+    image = pyvips.Image.new_from_file('drawimage/test.jpg')
+    txt = pyvips.Image.text('TEXT TO BE DISPLAYED', font='Iosevka', dpi=400)
+    image = image.insert(txt, 0, 0)
+    image.write_to_file('output.jpg')
+
 
 __benchmarks__ = [
     (draw_pillow, draw_imagemagick, "Drawing text using ImageMagick"),
     (draw_pillow, draw_gm, "Drawing text using GraphicsMagick"),
+    (draw_pillow, draw_vips, "Drawing text using pyvips"),
 ]
